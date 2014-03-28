287b045f9c5ab56c3a9debc5d0a89bf4f4d7d222
jason perez <jperezish@gmail.com>
2014-03-28 10:50:15 -0700

    Second scenario is passing with hard-coded html values

--- features/step_definitions/pool_steps.rb
+++ features/step_definitions/pool_steps.rb
@@ -1,6 +1,15 @@
 Given(/^there are no pools running today$/) do
 end
 
+Pool = Struct.new :status, :name
+Given(/^there are some pools running today$/) do
+  @pools = [
+    Pool.new("not_started", "Work Survivor Pool"),
+    Pool.new("not_started", "Family Survivor Pool"),
+    Pool.new("active", "The Elite Survivor Pool")
+  ]
+end
+
 When(/^I go to the running pools display page$/) do
   visit running_today_pools_url
 end
@@ -9,10 +18,10 @@ Then(/^I should see that there are no pools running$/) do
   page.should have_content("There are no pools running today")
 end
 
-Given(/^there are some pools running today$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
 Then(/^I should see the running pools grouped by status$/) do
-  pending # express the regexp above with the code you wish you had
+  @pools.each do |pool|
+    within(".#{pool.status}") do
+      page.should have_css(".pool", text: pool.name)
+    end
+  end
 end
