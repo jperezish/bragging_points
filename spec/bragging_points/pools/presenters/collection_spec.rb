c2aafdf35fc99cbbda8cfdef9fae668d98f83972
jason perez <jperezish@gmail.com>
2014-04-03 12:44:46 -0700

    Remove vestigial methods around getting pools in a specific status

--- spec/bragging_points/pools/presenters/collection_spec.rb
+++ spec/bragging_points/pools/presenters/collection_spec.rb
@@ -8,18 +8,6 @@ describe BraggingPoints::Pools::Presenters::Collection do
     let(:pools) { [not_started, active, complete] }
     let(:presenter) { BraggingPoints::Pools::Presenters::Collection.for(pools) }
 
-    it "exposes those in the not_started status" do
-      expect{ |b| presenter.not_started(&b) }.to yield_successive_args(not_started)
-    end
-
-    it "exposes those in the active status" do
-      expect{ |b| presenter.active(&b) }.to yield_successive_args(active)
-    end
-
-    it "exposes those in the complete status" do
-      expect{ |b| presenter.complete(&b) }.to yield_successive_args(complete)
-    end
-
     describe "#grouped_by_status" do
       it "yields the possible statuses for a pool" do
         expected_statuses = ["not_started", "active", "complete"]
