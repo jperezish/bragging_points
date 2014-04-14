f5756e0cfaca6bb9446c910cf617c59d41cfa551
jason perez <jperezish@gmail.com>
2014-04-14 12:54:36 -0700

    Update seed file, add pool spec stub

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Pool.create!([
+  {
+    name: "ABC Company Mega Pool",
+    status: "not started",
+    bragging_points: 30
+  },
+  {
+    name: "Best Buddies Pool",
+    status: "active",
+    bragging_points: 45
+  },
+  {
+    name: "Friends and Family Pool",
+    status: "not started",
+    bragging_points: 5
+  },
+  {
+    name: "Random Stranger Pool",
+    status: "not started",
+    bragging_points: 15
+  }
+])
--- spec/models/pool_spec.rb
+++ spec/models/pool_spec.rb
@@ -7,4 +7,11 @@ describe Pool do
     expect(pool.valid?).to be_false
     expect(pool.errors[:name].any?).to be_true
   end
+
+  it "requires a status"
+  it "requires bragging points"
+  it "rejects negative bragging points"
+  it "accepts 0 bragging points"
+  it "rejects any status that's not on the status list"
+  it "is valid with example attributes"
 end
