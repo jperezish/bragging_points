ab1e505c650a2892840159aa12222d2212c9fb8d
jason perez <jperezish@gmail.com>
2014-04-15 13:03:40 -0700

    Configure rspec with new defaults

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,16 @@ module BraggingPoints
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = false
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
