00a9b46055d30830b8bc7cd42e464bddb4053784
jason perez <jperezish@gmail.com>
2014-04-19 13:53:40 -0700

    Update rspec expectations in the pool spec

---
+++ features/twelve_digit_sku_spec.rb
@@ -0,0 +1,6 @@
+describe TwelveDigitSku do
+  it "is true" do
+    a = false
+    expect(a).to be_true
+  end
+end
--- spec/models/pool_spec.rb
+++ spec/models/pool_spec.rb
@@ -4,46 +4,50 @@ describe Pool do
   it "requires a name" do
     pool = Pool.new(name: "")
 
-    expect(pool.valid?).to be_false
-    expect(pool.errors[:name].any?).to be_true
+    expect(pool).to have(1).errors_on(:name)
   end
 
   it "requires a status" do
     pool = Pool.new(status: "")
 
-    expect(pool.valid?).to be_false
-    expect(pool.errors[:status].any?).to be_true
+    expect(pool).to have(2).errors_on(:status)
   end
 
   it "requires bragging points" do
     pool = Pool.new(bragging_points: "")
 
-    expect(pool.valid?).to be_false
-    expect(pool.errors[:bragging_points].any?).to be_true
+    expect(pool).to have(2).errors_on(:bragging_points)
   end
 
   it "rejects negative bragging points" do
     pool = Pool.new(bragging_points: -21)
 
-    expect(pool.valid?).to be_false
-    expect(pool.errors[:bragging_points].any?).to be_true
+    expect(pool).to have(1).errors_on(:bragging_points)
   end
 
   it "accepts 0 bragging points" do
     pool = Pool.new(bragging_points: 0)
 
-    expect(pool.valid?).to be_false
-    expect(pool.errors[:bragging_points].any?).to be_false
+    expect(pool).to have(0).errors_on(:bragging_points)
   end
 
-  it "rejects any status that's not on the status list" do
+  it "is valid with any status on the status list" do
     statuses = ["not started", "in process", "complete"]
 
     statuses.each do |status|
       pool = Pool.new(status: status)
 
-      expect(pool.valid?).to be_false
-      expect(pool.errors[:status].any?).to be_false
+      expect(pool).to have(0).errors_on(:status)
+    end
+  end
+
+  it "rejects any status that's not on the status list" do
+    statuses = ["notstarted", "inpocess", "omplete"]
+
+    statuses.each do |status|
+      pool = Pool.new(status: status)
+
+      expect(pool).to have(1).errors_on(:status)
     end
   end
   it "is valid with example attributes" do
